773cbd7f5f6681e6072559ed8c7d4a60b72366d8
hamdaxwar <editorberkelas2221@gmail.com>
2026-02-12 01:30:14 +0700

    Update sendrange.js

--- sendrange.js
+++ sendrange.js
@@ -115,7 +115,7 @@ async function startScraper() {
     });
 
     const context = await browser.newContext({
-    userAgent: 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0'
+    userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
 });
 
     try {
